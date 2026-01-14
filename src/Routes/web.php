1b751ebd4fc2b70ec3044c804b70c64c190c6a94
Kadiri <kadiri.moohamed@gmail.com>
2026-01-14 22:06:54 +0100

    feat: Add login route and enable router dispatch

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -1,15 +1,18 @@
 <?php
 
 use App\Routes\Router;
+use App\Controllers\LoginController;
 // session_start();
 
 /* Register routes */
-Router::get('/', __DIR__ . '/../app/Controllers/HomeController.php' , 'index');
+// Router::get('/', __DIR__ . '/../app/Controllers/HomeController.php' , 'index');
 
-echo "<pre>";
-var_dump(Router::$routes);
-echo "</pre>";
+// echo "<pre>";
+// var_dump(Router::$routes);
+// echo "</pre>";
 
+
+Router::get('/login', LoginController::class , 'login');
 
 /* Dispatch router */
-// Router::dispatch();
+Router::dispatch();
